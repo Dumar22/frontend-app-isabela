aec73cfc30c6af0754d16b79c2a25ab0483d2733
Dumar Fonseca <dumarfonseka@hotmail.com>
2024-02-13 11:35:41 -0500

    update routing

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -24,8 +24,8 @@ export class LoginComponent {
 
 
   public formulario: FormGroup = this.fb.group({
-    user: ['Admin', [Validators.required, Validators.minLength(5)]],
-    password: ['Admin12345', [Validators.required, Validators.minLength(6)]]   
+    user: ['', [Validators.required, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(6)]]   
   });
 
 
