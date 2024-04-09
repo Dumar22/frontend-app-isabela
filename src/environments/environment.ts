8cab55a3191a24261b10de45e19e84cc3a65a249
Dumar <dumarfonseka@hotmail.com>
2024-04-08 20:18:59 -0500

    update tools service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-    //base_url: 'https://nestjs-360.up.railway.app/api'
-   base_url: 'http://localhost:3030/api'
+    base_url: 'https://nestjs-360.up.railway.app/api'
+  // base_url: 'http://localhost:3030/api'
   // base_url: 'http://190.0.14.109:3030/api'
 };
 
